967c62922b5377c5ebfba8c4d6636dbb7b048f7d
denis <denis@kaizen-design.ru>
2023-01-31 03:24:45 +0300

    Navbar component created

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,9 +6,31 @@ import styles from '../styles';
 import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8 relative`}>
+      <img
+        src="/search.svg"
+        alt="Search"
+        className="w-[24px] h-[24px] object-contain cursor-pointer"
+      />
+      <h2
+        className="font-extrabold text-[24px] leading-[30px] text-white"
+      >
+        METAVERSUS
+      </h2>
+      <img
+        src="/menu.svg"
+        alt="Menu"
+        className="w-[24px] h-[24px] object-contain cursor-pointer"
+      />
+    </div>
+  </motion.nav>
 );
 
 export default Navbar;
